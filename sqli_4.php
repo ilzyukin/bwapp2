8de8e021ebf3cf965d86041cda191785daed51f8
Filia35 <56798858+Filia35@users.noreply.github.com>
2019-12-07 15:57:46 +0300

    SQL_1

--- sqli_4.php
+++ sqli_4.php
@@ -22,6 +22,52 @@ include("selections.php");
 include("functions_external.php");
 include("connect.php");
 
+$temp = setlocale(LC_ALL, "ru_RU.utf8");
+$temp =bindtextdomain("messages", "./var/www/html/bWAPP/lang/ru_RU/LC_MESSAGES");
+
+
+
+//___________________
+
+$temp3 = _("bWAPP - SQL Injection"); // echo($temp3) bWAPP - SQL инъекция
+$temp4 = _("an extremely buggy web app !");
+$temp5 = _("New user");// echo($temp5) Новый пользователь
+$temp6 = _("Information");// echo($temp6) Информация
+$temp7 = _("Training");//  echo($temp7) Тренировочные задания
+$temp8 = _("Blog");///  echo($temp8) Блог
+$temp9 =  _("Enter your credentials");//echo($temp9) Введите ваши данные
+$temp10 =  _("password");// echo($temp10)        Пароль
+$temp11 = _("Check security_level");//echo($temp11)        Установите уровень безопасности:
+$temp12 =  _("low");//echo($temp12)   низкий
+$temp13 =  _("middle");//echo($temp12)   средний
+$temp14 =  _("high");// echo($temp14)   высокий
+$temp15 =  _("Bugs");// echo($temp15)   баги
+$temp16 =  _("Change Password");// echo($temp16)   Изменить пароль
+$temp17 =  _("Create User");// echo($temp17)   Создать пользователя
+$temp18 =  _("Set Security Level");// echo($temp18)   Установить уровень безопасности
+$temp19 =  _("Reset");// echo($temp19)   Сбросить
+$temp20 =  _("Credits");// echo($temp20)   Контактные данные
+$temp21 =  _("Logout");// echo($temp21)   Выйти из аккаунта
+$temp22 =  _("Welcome");// echo($temp22)   Добро пожаловать
+$temp23 =  _("Broken Auth. - Password Attacks");// echo($temp23)   атаки на пароль
+$temp24 =  _("Login:");// echo($temp24)   Логин
+$temp25 =  _("Password:");// echo($temp25)   Пароль
+$temp26 =  _("Re-enter CAPTCHA:");// echo($temp26)   Повторно введите капчу
+$temp27 =  _("Login");// echo($temp27)   Логин
+$temp28 =  _("bWAPP is licensed under");// echo($temp28)   Лицензия БИВАП от
+$temp29 =  _("2014 MME BVBA / Follow");// echo($temp29)   2014 MME BVBA / Подписывайтесь
+$temp30 =  _("on Twitter and ask for our cheat sheet, containing all solutions! / Need an exclusive ");// echo($temp30)   
+$temp31 =  _("training");// echo($temp31)   Тренировка
+$temp32 =  _("Set your security level:");// echo($temp32)   Введите Ваш уровень безопасности
+$temp33 =  _("medium");// echo($temp33)   средний
+$temp34 =  _("Set");// echo($temp34)   Установить
+$temp35 =  _("Current:");// echo($temp35)   Текущий
+$temp36 =  _("Enter your credentials.");// echo($temp36)   Введите Ваши данные
+$temp37 =  _("SQL Injection - Blind - Boolean-Based");//   
+$temp38 =  _("Search for a movie:");//    
+$temp39 =  _("Search");//    
+ 
+
 function sqli($data)
 {
 
@@ -69,7 +115,7 @@ function sqli($data)
 <!--<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>-->
 <script src="js/html5.js"></script>
 
-<title>bWAPP - SQL Injection</title>
+<title><?php echo($temp3) ?></title>
 
 </head>
 
@@ -79,7 +125,7 @@ function sqli($data)
 
 <h1>bWAPP</h1>
 
-<h2>an extremely buggy web app !</h2>
+<h2><?php echo($temp4) ?></h2>
 
 </header>
 
@@ -89,15 +135,15 @@ function sqli($data)
 
         <tr>
 
-            <td><a href="portal.php">Bugs</a></td>
-            <td><a href="password_change.php">Change Password</a></td>
-            <td><a href="user_extra.php">Create User</a></td>
-            <td><a href="security_level_set.php">Set Security Level</a></td>
-            <td><a href="reset.php" onclick="return confirm('All settings will be cleared. Are you sure?');">Reset</a></td>
-            <td><a href="credits.php">Credits</a></td>
-            <td><a href="http://itsecgames.blogspot.com" target="_blank">Blog</a></td>
-            <td><a href="logout.php" onclick="return confirm('Are you sure you want to leave?');">Logout</a></td>
-            <td><font color="red">Welcome <?php if(isset($_SESSION["login"])){echo ucwords($_SESSION["login"]);}?></font></td>
+            <td><a href="portal.php"><?php echo($temp15) ?></a></td>
+            <td><a href="password_change.php"><?php echo($temp16) ?></a></td>
+            <td><a href="user_extra.php"><?php echo($temp17) ?></a></td>
+            <td><a href="security_level_set.php"><?php echo($temp18) ?></a></td>
+            <td><a href="reset.php" onclick="return confirm('Все настройки будут сброшены. Вы уверены?');"><?php echo($temp19) ?></a></td>
+            <td><a href="credits.php"><?php echo($temp20) ?></a></td>
+            <td><a href="http://itsecgames.blogspot.com" target="_blank"><?php echo($temp8) ?></a></td>
+            <td><a href="logout.php" onclick="return confirm('Вы уверены, что хотите выйти?');"><?php echo($temp21) ?></a></td>
+            <td><font color="red"><?php echo($temp22) ?> <?php if(isset($_SESSION["login"])){echo ucwords($_SESSION["login"]);}?></font></td>
 
         </tr>
 
@@ -107,16 +153,16 @@ function sqli($data)
 
 <div id="main">
 
-    <h1>SQL Injection - Blind - Boolean-Based</h1>
+    <h1><?php echo($temp37) ?></h1>
 
     <form action="<?php echo($_SERVER["SCRIPT_NAME"]); ?>" method="GET">
 
         <p>
 
-        <label for="title">Search for a movie:</label>
+        <label for="title"><?php echo($temp38) ?></label>
         <input type="text" id="title" name="title" size="25">
 
-        <button type="submit" name="action" value="search">Search</button>
+        <button type="submit" name="action" value="search"><?php echo($temp39) ?></button>
 
         </p>
 
@@ -135,7 +181,7 @@ function sqli($data)
         if(!$recordset)
         {
 
-            die("<font color=\"red\">Incorrect syntax detected!</font>");
+            die("<font color=\"red\">Обнаружен неверный синтаксис!</font>");
             // die("Error: " . mysqli_error());
 
         }
@@ -143,14 +189,14 @@ function sqli($data)
         if(mysqli_num_rows($recordset) != 0)
         {
 
-            echo "The movie exists in our database!";
+            echo "Фильм существует в нашей базе данных!";
 
         }
 
         else
         {
 
-            echo "The movie does not exist in our database!";
+            echo "Фильм не существует в нашей базе данных!";
 
         }
 
@@ -172,7 +218,7 @@ function sqli($data)
 
 <div id="disclaimer">
 
-    <p>bWAPP is licensed under <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank"><img style="vertical-align:middle" src="./images/cc.png"></a> &copy; 2014 MME BVBA / Follow <a href="http://twitter.com/MME_IT" target="_blank">@MME_IT</a> on Twitter and ask for our cheat sheet, containing all solutions! / Need an exclusive <a href="http://www.mmebvba.com" target="_blank">training</a>?</p>
+    <p><?php echo($temp28) ?> <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank"><img style="vertical-align:middle" src="./images/cc.png"></a> &copy; <?php echo($temp29) ?> <a href="http://twitter.com/MME_IT" target="_blank">@MME_IT</a> <?php echo($temp30) ?> <a href="http://www.mmebvba.com" target="_blank"><?php echo($temp31) ?></a>?</p>
 
 </div>
 
@@ -186,18 +232,18 @@ function sqli($data)
 
     <form action="<?php echo($_SERVER["SCRIPT_NAME"]);?>" method="POST">
 
-        <label>Set your security level:</label><br />
+        <label><?php echo($temp32) ?></label><br />
 
         <select name="security_level">
 
-            <option value="0">low</option>
-            <option value="1">medium</option>
-            <option value="2">high</option>
+            <option value="0"><?php echo($temp12) ?></option>
+            <option value="1"><?php echo($temp33) ?></option>
+            <option value="2"><?php echo($temp14) ?></option>
 
         </select>
 
-        <button type="submit" name="form_security_level" value="submit">Set</button>
-        <font size="4">Current: <b><?php echo $security_level?></b></font>
+        <button type="submit" name="form_security_level" value="submit"><?php echo($temp34) ?></button>
+        <font size="4"><?php echo($temp35) ?> <b><?php echo $security_level?></b></font>
 
     </form>
 
