42ac42f665a6172e50d6beae3038ba94501a247e
Vakula <kissingtheshadow9@gmail.com>
2019-12-24 21:28:36 +0300

    xss_1

--- xss_href-2.php
+++ xss_href-2.php
@@ -157,10 +157,10 @@ function hpp($data)
             <td><a href="password_change.php"><?php echo($temp5) ?></a></td>
             <td><a href="user_extra.php"><?php echo($temp7) ?></a></td>
             <td><a href="security_level_set.php"><?php echo($temp8) ?></a></td>
-            <td><a href="reset.php" onclick="return confirm('All settings will be cleared. Are you sure?');"><?php echo($temp14) ?></a></td>
+            <td><a href="reset.php" onclick="return confirm('Все настройки будут сброшены. Вы уверены?');"><?php echo($temp14) ?></a></td>
             <td><a href="credits.php"><?php echo($temp9) ?></a></td>
             <td><a href="http://itsecgames.blogspot.com" target="_blank"><?php echo($temp10); ?></a></td>
-            <td><a href="logout.php" onclick="return confirm('Are you sure you want to leave?');"><?php echo($temp11); ?></a></td>
+            <td><a href="logout.php" onclick="return confirm('Вы уверены, что хотите выйти?');"><?php echo($temp11); ?></a></td>
             <td><font color="red"><?php echo($temp12); ?> <?php if(isset($_SESSION["login"])){echo ucwords($_SESSION["login"]);}?></font></td>
 
         </tr>
